d0517f05a70bdf6570b7d916af7bbd8e7a04dd43
Xinyi Zhuang <yingmu52@msn.com>
2018-03-08 00:04:43 -0800

    Update Firebase Cloud Function

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+var request = require('request');
+const webhook = ""; // :)
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Free Plan this function will not work
 exports.passedTest = functions.database.ref('/Word').onCreate((event) => {
-  console.log("Data saved");
+  var message = {
+    text: "伟琳刚刚背完了今天的单词"
+  };
+  request({
+    url: webhook,
+    method: "POST",
+    json: true,
+    body: message
+  }, (error, response, body) => {
+    if (error === null || error === undefined) {
+      return console.log("Lin passed a test");
+    } else {
+      return console.log(error);
+    }
+  });
 });
